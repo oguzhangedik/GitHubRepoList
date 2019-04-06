47bdc1637acc5cec94b67e0e67ca93468a3ca461
oguzhangedik <info.oguzhangedik@gmail.com>
2019-04-07 00:04:08 +0300

    Start to add listing processes

--- GithubRepoListApp/app/src/main/java/com/ingbank/mobileapp/githubrepolist/githubrepolist/activities/RepoListActivity.java
+++ GithubRepoListApp/app/src/main/java/com/ingbank/mobileapp/githubrepolist/githubrepolist/activities/RepoListActivity.java
@@ -1,28 +1,50 @@
 package com.ingbank.mobileapp.githubrepolist.githubrepolist.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.ingbank.mobileapp.githubrepolist.githubrepolist.R;
+import com.ingbank.mobileapp.githubrepolist.githubrepolist.adapters.MyRecyclerViewAdapter;
 import com.ingbank.mobileapp.githubrepolist.githubrepolist.httpclient.RestInterfaceController;
 import com.ingbank.mobileapp.githubrepolist.githubrepolist.httpclient.RetrofitClient;
 import com.ingbank.mobileapp.githubrepolist.githubrepolist.httpclient.models.outputs.Repo;
 import com.ingbank.mobileapp.githubrepolist.githubrepolist.utils.InternetConnectionUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class RepoListActivity extends AppCompatActivity {
+public class RepoListActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener{
 
+    private MyRecyclerViewAdapter adapter;
+    private ArrayList<String> animalNames;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repo_list);
 
+
+        // data to populate the RecyclerView with
+        animalNames = new ArrayList<>();
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.rvAnimals);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        MyRecyclerViewAdapter adapter = new MyRecyclerViewAdapter(this, animalNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
 
         if (!InternetConnectionUtil.isConnected()) {
             Toast.makeText(this, "Check your network connn", Toast.LENGTH_LONG).show();
@@ -46,5 +68,10 @@ public class RepoListActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + animalNames.get(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 }
